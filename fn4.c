9b23539da6cb3a5db4d86e47aba6a2ebb0b12de0
Santiago Mercado <santymc@icloud.com>
2022-07-07 23:13:14 -0300

    progresd 4

---
+++ fn4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int can=1;
+    double mont[can], total=0;
+    char mp[can];
+    for(int i=0;i<can;i++){
+        do{
+            puts("Ingrese el medio de pago (E=efectivo, D=debito, C=credito)");
+            scanf("%lf", &mont[i]);
+        } while(mont[i]!=0 && mont[i]<0);
+        if
+    }
+    
+}
---
+++ fn5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define CAN 10
+
+int main() {
+    int arr[CAN];
+
+    return 0;
+}
