5ba4eb2406314e9823b6ed1426c3dc7527b0d61c
laptopSanty <[santymc@icloud.com](mailto:santymc@icloud.com)>
2022-08-10 10:40:18 -0300

    consigna actividad 4

---
+++ PROGRAM_I/act4.c
@@ -0,0 +1,6 @@
+/*Realizar un programa para anotarse al final de julio donde:
+● se lea una CADENA DE CARACTERES, ingresando por teclado la materiaque se quiere anotar (INTRO, PROGRA)
+● determine si lo ingresado es VÀLIDO o NO, lo avise por pantalla y vuelva aingresar hasta que sea válido.
+Permitir el ingreso de mayúsculas y minúsculas.Sólo una inscripción.
+pista: Función para validar la materia*/
+
