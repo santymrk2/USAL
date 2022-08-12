963eefc37cc655a3cceaf1d4598a48a2856a1c4b
Santiago Mercado <santymc@icloud.com>
2022-08-12 06:37:11 -0300

    Nuevo commit

--- PROGRAM_I/act4.c
+++ PROGRAM_I/act4.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main() {
-	return 0;
-}
-
 /*Realizar un programa para anotarse al final de julio donde:
 ● se lea una CADENA DE CARACTERES, ingresando por teclado la materiaque se quiere anotar (INTRO, PROGRA)
 ● determine si lo ingresado es VÀLIDO o NO, lo avise por pantalla y vuelva aingresar hasta que sea válido.
 Permitir el ingreso de mayúsculas y minúsculas.Sólo una inscripción.
 pista: Función para validar la materia*/
+
+int main() {
+	return 0;
+}
+
 
