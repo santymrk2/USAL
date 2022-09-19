7af3b84052a65b617549c3bcdf38a8aeca3b2590
laptopSanty <[santymc@icloud.com](mailto:santymc@icloud.com)>
2022-09-18 22:06:53 -0300

    ejercicio 33 empezado

---
+++ PROGRAM_I/E033.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+Para la misma entidad deportiva y utilizando la misma tabla de costos anuales de membresía, desarrollar una función que:
+● reciba la tabla CARGADA en forma desordenada y una categoría de socio (member type)
+● devuelva los datos asociados a esa categoría
+*/
+
+void dataByType(memberT m[], char l[]){
+    //recorrer las categorias y encontrar l
+    for(int i=0; i<strlen(m); i++){
+        if(strlen(m.name, l)==0){
+            i=strlen(m);
+        } else {}
+    }
+    //devolver datos asociados
+    return;
+}
